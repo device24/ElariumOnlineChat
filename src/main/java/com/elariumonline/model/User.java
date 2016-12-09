09191be61859f56e5e4c0cd307bc70ec3548436e
device24 <belousov_2010@list.ru>
2016-12-09 21:43:51 +0500

    add model User.java

---
+++ src/main/java/com/elariumonline/model/User.java
@@ -0,0 +1,28 @@
+package com.elariumonline.model;
+
+public class User {
+
+    public String login;
+
+    public String password;
+
+    public User() {
+    }
+
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
